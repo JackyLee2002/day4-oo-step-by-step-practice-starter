4340d092384bc585c03273201d24601b4c025a49
Jacky Lee <leeja12@oocl.com>
2024-11-21 13:47:44 +0800

    feat: AC2 completed Teacher and TeacherTest

--- src/main/java/oo/Teacher.java
+++ src/main/java/oo/Teacher.java
@@ -1,4 +1,21 @@
 package oo;
 
-public class Teacher {
+import java.util.Objects;
+
+public class Teacher extends Person {
+    public Teacher(Integer id, String name, Integer age) {
+        super(id, name, age);
+    }
+
+    public String introduce() {
+        return String.format("My name is %s. I am %d years old. I am a teacher.", name, age);
+    }
+
+    @Override
+    public boolean equals(Object objectToBeCompared) {
+        if (this == objectToBeCompared) return true;
+        if (objectToBeCompared == null || getClass() != objectToBeCompared.getClass()) return false;
+        Person objectCastedAsPerson = (Person) objectToBeCompared;
+        return Objects.equals(this.id, objectCastedAsPerson.id);
+    }
 }

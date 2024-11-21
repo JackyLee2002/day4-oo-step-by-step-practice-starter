5ecf3176ed893c5d3ce7229bdbff073aa34ca918
Jacky Lee <leeja12@oocl.com>
2024-11-21 13:39:17 +0800

    feat: AC1 Person Class and override equals check

--- src/main/java/oo/Person.java
+++ src/main/java/oo/Person.java
@@ -1,4 +1,32 @@
 package oo;
 
+import java.util.Objects;
+
 public class Person {
+    private Integer id;
+
+    private String name;
+
+    private Integer age;
+
+    public Person(Integer id, String name, Integer age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public String introduce() {
+        return String.format("My name is %s. I am %d years old.", name, age);
+    }
+
+    @Override
+    public boolean equals(Object objectToBeCompared) {
+        if (this == objectToBeCompared) return true;
+        if (objectToBeCompared == null || getClass() != objectToBeCompared.getClass()) return false;
+        Person objectCastedAsPerson = (Person) objectToBeCompared;
+        return Objects.equals(this.id, objectCastedAsPerson.id);
+    }
+
+
+
 }

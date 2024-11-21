0a5eac6fadc4faf549ca980cfbecc1c31f93b71b
Jacky Lee <leeja12@oocl.com>
2024-11-21 14:11:19 +0800

    feat: AC4 assignTo, belongsTo and introduce testcases

--- src/main/java/oo/Teacher.java
+++ src/main/java/oo/Teacher.java
@@ -1,26 +1,34 @@
 package oo;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public class Teacher extends Person {
+
+    private List<Klass> klass = new ArrayList<>();
+
     public Teacher(Integer id, String name, Integer age) {
         super(id, name, age);
     }
 
-    public String introduce() {
-        return String.format("My name is %s. I am %d years old. I am a teacher.", name, age);
+    @Override
+    public String introduce(){
+        String teacherIntroduction = String.format("My name is %s. I am %d years old. I am a teacher.", name, age);
+        if(!Objects.isNull(klass) && !klass.isEmpty()){
+            teacherIntroduction += String.format(" I teach Class %s.", String.join(", ",klass.stream().map(i -> String.valueOf(i.getNumber())).toArray(String[]::new)));
+        }
+        return teacherIntroduction;
     }
 
-    @Override
-    public boolean equals(Object objectToBeCompared) {
-        if (this == objectToBeCompared) return true;
-        if (objectToBeCompared == null || getClass() != objectToBeCompared.getClass()) return false;
-        Person objectCastedAsPerson = (Person) objectToBeCompared;
-        return Objects.equals(this.id, objectCastedAsPerson.id);
+
+    public void assignTo(Klass klass) {
+        if(this.klass.stream().noneMatch(i -> i.equals(klass))) {
+            this.klass.add(klass);
+        }
     }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
+    public boolean belongsTo(Klass klass) {
+        return !this.klass.isEmpty() && !Objects.isNull(klass) && this.klass.contains(klass);
     }
 }

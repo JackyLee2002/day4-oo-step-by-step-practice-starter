eb6fa369000e222b7c554abd19749c23901f9b6d
Jacky Lee <leeja12@oocl.com>
2024-11-21 13:43:38 +0800

    feat: AC2 test pass (should_return_message_with_name_and_age_when_introduce_given_student)

--- src/main/java/oo/Student.java
+++ src/main/java/oo/Student.java
@@ -1,4 +1,22 @@
 package oo;
 
-public class Student {
+import java.util.Objects;
+
+public class Student extends Person{
+
+    public Student(Integer id, String name, Integer age) {
+        super(id, name, age);
+    }
+
+    public String introduce() {
+        return String.format("My name is %s. I am %d years old. I am a student.", name, age);
+    }
+
+    @Override
+    public boolean equals(Object objectToBeCompared) {
+        if (this == objectToBeCompared) return true;
+        if (objectToBeCompared == null || getClass() != objectToBeCompared.getClass()) return false;
+        Person objectCastedAsPerson = (Person) objectToBeCompared;
+        return Objects.equals(this.id, objectCastedAsPerson.id);
+    }
 }
